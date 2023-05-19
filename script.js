57dce22d28d29baa38b8066e9cb55f8ce8ae6d33
Vatsal <rajvatsal2004@gmail.com>
2023-05-19 17:58:29 +0530

    MAJOR: Make the number of DOM nodes user defined
    It will create a new function createSquares which will contain the block
    of code responsible for creating squares which are now user defined.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let body = document.querySelector('body');
-let rowGroup = document.createElement('div');
-rowGroup.setAttribute('id', 'row-group');
-body.appendChild(rowGroup);
-for(let i = 0; i < 4; i++){
-    let row = document.createElement('div');
-    row.classList.add('row');
-    rowGroup.appendChild(row);
-    for(let j = 0; j < 4; j++){
-        let square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
+function createSquares(rowNum, columnNum){
+    let body = document.querySelector('body');
+    let rowGroup = document.createElement('div');
+    rowGroup.setAttribute('id', 'row-group');
+    body.appendChild(rowGroup);
+    for(let i = 0; i < rowNum; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        rowGroup.appendChild(row);
+        for(let j = 0; j < columnNum; j++){
+            let square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
     }
 }
