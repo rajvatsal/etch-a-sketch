d4d560a84213dec4fc2d51ac4eb2b70fd9f2cecb
Vatsal <rajvatsal2004@gmail.com>
2023-05-20 11:25:32 +0530

    MAJOR: Add a button 'eraser'

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function createSquares(e){
+function generateStylus(e){
     e.stopPropagation();
     clearMain();
+
     let rowNum = prompt("enter row number");
     let columnNum = prompt("enter column number");
     let main = document.querySelector('main');
     let rowGroup = document.createElement('div');
+
     rowGroup.setAttribute('id', 'row-group');
     main.appendChild(rowGroup);
+
     for (let i = 0; i < rowNum; i++){
         let row = document.createElement('div');
         row.classList.add('row');
@@ -17,13 +20,29 @@ function createSquares(e){
             row.appendChild(square);
         }
     }
+    drawInStylus();
 }
 function clearMain(){
     let main = document.querySelector('main');
-    console.log(main);
     while (main.hasChildNodes()){
         main.removeChild(main.firstChild);        
     }
 }
-let button = document.querySelector('button');
-button.addEventListener('click', createSquares);
+function drawInStylus(){
+    let square = document.querySelectorAll('.square');
+
+    square.forEach(singleSquare => singleSquare.addEventListener('mouseover', (e) => {
+        e.target.classList.add('draw');
+    }))
+}
+function eraseInStylus(){
+    let square = document.querySelectorAll('.square');
+    square.forEach(singleSquare => singleSquare.addEventListener('mouseover', (e) => {
+        e.target.classList.remove('draw');
+    }))
+}
+let generateStylusButton = document.querySelector('.generate-stylus');
+let eraseInStylusButton = document.querySelector('.eraser');
+
+generateStylusButton.addEventListener('click', generateStylus);
+eraseInStylusButton.addEventListener('click', eraseInStylus);
